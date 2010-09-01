171c18ef6a3963a0d7c50ae906b0ae3ef6431745
Ben Barnett <ben@brushfiredesign.net>
2010-09-01 16:13:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+jquery.animate-enhanced plugin
+
+Extend $.animate() to detect CSS transitions for Webkit, Mozilla and Opera and convert animations automatically. Compatible with IE6+
+
+Properties supported: (more to come)
+
+left : using translate(x, y) or translate3d(x, y, z)
+top : using translate(x, y) or translate3d(x, y, z)
+opacity
+
+What it does
+
+The plugin will analyse the properties you're animating on, and select the most appropriate method for the browser in use. This means your transitions on left, top and opacity will convert to a CSS3 transition on Webkit & Mozilla agents that support it, and Opera 10.50+. If the user is on a browser that has no CSS3 transitions, this plugin knows about it and won't get involved. Silent degradation :)
+
+Multiple callback mechanisms are created internally to monitor for DOM manipulation and for all 'transitionend' CSS3 events to be picked up. This means you have one neat callback() for the whole animation regardless on whether the plugin is using CSS3, DOM, or both for its animations.
+
+Progressively enhanced CSS3 animations without having to do any browser detection or special CSS, therefore using the same Javascript across your applications and websites.
+
+As this plugin uses CSS3 translate where available, there is an internal callback mechanism to reset the 'left' and/or 'top' properties so that your layout is unaffected.
+
+Usage
+
+Usage is identical to the jQuery animate() function, but it comes with 3 new paramaters, which are totally optional and safe to leave untouched for general use:
+
+avoidTransforms: (Boolean)
+By default the plugin will convert left and top animations to the CSS3 style -webkit-transform (or equivalent) to aid hardware acceleration. This functionality can be disabled by setting this to true.
+useTranslate3d: (Boolean)
+By default the plugin will use 2d translations due to wider browser support. Set this to true to use translate3d instead. Recommended for iPhone/iPad development (here's why).
+leaveTransforms: (Boolean)
+By default if the plugin is animating a left or a top property, the translate (2d or 3d depending on setting above) CSS3 transformation will be used. To preserve other layout dependencies, once the transition is complete, these transitions are removed and converted back to the real left and top values. Set this to true to skip this functionality.
+Credits
+
+Author: Ben Barnett - http://www.benbarnett.net - @benpbarnett
+HeyDanno! for his tips on detecting CSS3 support
+Aza for his gist on CSS Animation wtih jQuery
---
+++ scripts/src/jquery.animate-enhanced.js
@@ -0,0 +1,201 @@
+/************************************************
+	jquery.animate-enhanced plugin v0.1
+	Author: www.benbarnett.net || @benpbarnett
+*************************************************
+
+Extends $.animate() to automatically use CSS3 transformations where applicable.
+Requires jQuery 1.4.2+
+
+Supports -moz-transition, -webkit-transition, -o-transition, transition
+	
+Targetted properties (for now):
+	- left
+	- top
+	- opacity
+	
+Usage (exactly the same as it would be normally):
+	
+	$(element).animate({left: 200},  500, function() {
+		// callback
+	});
+	
+*********/
+
+(function($) {
+	// ----------
+	// Plugin variables
+	// ----------
+	var cssTransitionsSupported = false,
+		originalAnimateMethod = $.fn.animate,
+		rupper = /([A-Z])/g,
+		cssTransitionProperties = ["top", "left", "opacity"],
+		cssPrefixes = ["", "-webkit-", "-moz", "-o"],
+		callbackQueue = 0,
+		cssMeta = {};
+		
+		
+	
+	// ----------
+	// Check if this browser supports CSS3 transitions
+	// ----------
+	(function() {
+	    var div = document.createElement('div');
+	    div.setAttribute('style', 'transition:top 1s ease;-webkit-transition:top 1s ease;-moz-transition:top 1s ease;-o-transition:top 1s ease;');
+	    cssTransitionsSupported = !!(div.style.transition || div.style.webkitTransition || div.style.MozTransition || div.style.OTransition);
+	    delete div;
+	})();
+	
+	
+	// ----------
+	// Make a translate or translate3d string
+	// ----------
+	$.fn.getTranslation = function(x, y, use3D) {
+		return (use3D === true) ? "translate3d("+x+"px,"+y+"px,0)" : "translate("+x+"px,"+y+"px)";
+	};
+	
+	
+	
+	// ----------
+	// Build up the CSS object
+	// ----------
+	$.fn.applyCSSTransition = function(cssProperties, property, duration, easing, value, isTransform, use3D) {		
+		if (property == "left" || property == "top") cssMeta[property] = value;
+		return $.fn.applyCSSWithPrefix(cssProperties, property, duration, easing, value, isTransform, use3D);
+	};
+	
+	// ----------
+	// Helper function to build up CSS properties using the various prefixes
+	// ----------
+	$.fn.applyCSSWithPrefix = function(cssProperties, property, duration, easing, value, isTransform, use3D) {
+		for (var i = cssPrefixes.length - 1; i >= 0; i--){			
+			if (typeof cssProperties[cssPrefixes[i] + 'transition-property'] === 'undefined') cssProperties[cssPrefixes[i] + 'transition-property'] = '';
+			cssProperties[cssPrefixes[i]+'transition-property'] += ', ' + ((isTransform === true) ? cssPrefixes[i] + 'transform' : property);
+			cssProperties[cssPrefixes[i]+'transition-duration'] = duration + 'ms';
+			cssProperties[cssPrefixes[i]+'transition-timing-function'] = easing;
+			cssProperties[((isTransform === true) ? cssPrefixes[i]+'transform' : property)] = (isTransform === true) ? $.fn.getTranslation(cssMeta.left, cssMeta.top, use3D) : value;
+		};
+		
+		return cssProperties;
+	};
+	
+	
+	// ----------
+	// The new $.animate() function
+	// ----------
+	$.fn.animate = function(prop, speed, easing, callback) {
+		if (!cssTransitionsSupported || $.isEmptyObject(prop)) return originalAnimateMethod.apply(this, arguments);
+		
+		var opt = speed && typeof speed === "object" ? speed : {
+			complete: callback || !callback && easing ||
+				jQuery.isFunction( speed ) && speed,
+			duration: speed,
+			easing: callback && easing || easing && !jQuery.isFunction(easing) && easing
+		}, propertyCallback = function() {
+			callbackQueue--;
+			if (callbackQueue <= 0) { 
+				// clean up the animation props
+				$(this).each(function() {
+					$(this).css({
+						'-moz-transition-property': 'none',  
+						'-moz-transition-duration': '',  
+						'-moz-transition-timing-function': '',
+						'-webkit-transition-property': 'none',  
+						'-webkit-transition-duration': '',  
+						'-webkit-transition-timing-function': '',
+						'-o-transition-property': 'none',  
+						'-o-transition-duration': '',  
+						'-o-transition-timing-function': '',
+						'transition-property': 'none',  
+						'transition-duration': '',  
+						'transition-timing-function': ''
+					});
+				});
+				
+				// convert translations to left & top for layout
+				if (!prop.leaveTransforms === true) {
+					if (cssMeta.top !== 0) {
+						$(this).each(function(){
+							$(this).css({
+								'-webkit-transform': 'translate(0, 0)',
+								'-moz-transform': 'translate(0, 0)',
+								'-o-transform': 'translate(0, 0)',
+								'transform': 'translate(0, 0)',
+								'top': cssMeta.top + 'px'
+							});
+						});
+					}
+					if (cssMeta.left !== 0) {
+						$(this).each(function(){
+							$(this).css({
+								'-webkit-transform': 'translate(0, 0)',
+								'-moz-transform': 'translate(0, 0)',
+								'-o-transform': 'translate(0, 0)',
+								'transform': 'translate(0, 0)',
+								'left': cssMeta.left + 'px'
+							});
+						});
+					}
+				}
+				
+				
+				// we're done, trigger the user callback
+				return opt.complete.call();
+			}
+		},
+		easings = {
+			bounce: 'cubic-bezier(0.0, 0.35, .5, 1.3)', 
+			linear: 'linear',
+			swing: 'ease-in-out'
+		},
+		cssProperties = {}, domProperties = null, cssEasing = "";
+		cssMeta.top = 0; cssMeta.left = 0;
+		
+		// make easing css friendly
+		cssEasing = opt.easing || "swing";
+		cssEasing = easings[cssEasing] ? easings[cssEasing] : cssEasing;
+
+		// seperate out the properties for the relevant animation functions
+		for (p in prop) {
+			if ($.inArray(p, cssTransitionProperties) > -1) {
+				$.fn.applyCSSTransition(
+					cssProperties, 
+					p, 
+					opt.duration, 
+					cssEasing, 
+					prop[p], 
+					(((p == "left" || p == "top") && prop.avoidTransforms !== true) ? true : false), 
+					(prop.useTranslate3d === true) ? true : false);
+					
+				continue;
+			}	
+			if (p !== 'useTranslate3d' && p !== 'avoidTransforms' && p !== 'leaveTransforms') {
+				domProperties = (!domProperties) ? {} : domProperties;
+				domProperties[p] = prop[p];
+			}
+		}
+		
+		// clean up
+		delete cssProperties.internals;
+		for (var i = cssPrefixes.length - 1; i >= 0; i--){
+			if (typeof cssProperties[cssPrefixes[i]+'transition-property'] !== 'undefined') cssProperties[cssPrefixes[i]+'transition-property'] = cssProperties[cssPrefixes[i]+'transition-property'].substr(2);
+		}
+
+		// fire up DOM based animations
+		if (domProperties) {
+			callbackQueue++;
+			originalAnimateMethod.apply(this, [domProperties, opt.duration, opt.easing, propertyCallback]);
+		}
+		
+		// apply the CSS transitions
+		this.each(function() {
+			callbackQueue++;
+			
+			// TODO: Make this less browser specific, this will have to do for now
+			$(this).bind(($.browser.webkit) ? 'webkitTransitionEnd' : 'transitionend', propertyCallback);
+			$(this).css(cssProperties);
+		});
+		
+		// over and out
+		return this;
+	};	
+})(jQuery);
