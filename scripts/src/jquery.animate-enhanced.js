773510724a6592e300089364b244e013efa29338
Ben Barnett <ben@brushfiredesign.net>
2010-12-22 03:07:57 -0800

    v0.54a license changed to MIT

--- scripts/src/jquery.animate-enhanced.js
+++ scripts/src/jquery.animate-enhanced.js
@@ -1,8 +1,30 @@
-/************************************************
-	jquery.animate-enhanced plugin v0.54
-	Author: www.benbarnett.net || @benpbarnett
-*************************************************
+/*
+jquery.animate-enhanced plugin v0.54
+---
+http://github.com/benbarnett/jQuery-Animate-Enhanced
+http://benbarnett.net
+@benpbarnett
+---
+Copyright (c) 2010 Ben Barnett
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+---
 Extends jQuery.animate() to automatically use CSS3 transformations where applicable.
 Requires jQuery 1.4.2+
 
@@ -22,6 +44,9 @@ Usage (exactly the same as it would be normally):
 	});
 	
 Changelog:
+	0.54a (22/12/2010):
+		- License changed to MIT (http://www.opensource.org/licenses/mit-license.php)
+
 	0.54 (22/12/2010):
 		- Removed silly check for 'jQuery UI' bailouts. Sorry.
 		- Scoping issues fixed - Issue #4: $(this) should give you a reference to the selector being animated.. per jquery's core animation funciton.
@@ -72,9 +97,7 @@ Changelog:
 		- Applies data[translateX] and data[translateY] to elements for easy access
 		- Added 'easeInOutQuint' easing function for CSS transitions (requires jQuery UI for JS anims)
 		- Less need for leaveTransforms = true due to better position detections
-
-	
-*********/
+*/
 
 (function($, originalAnimateMethod, originalStopMethod) {
 	// ----------
