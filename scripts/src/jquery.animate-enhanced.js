4e6aa80be2dd7bcc0e5278250dea288016139d80
Dan Auclair <daniel.auclair@gmail.com>
2011-03-30 13:13:09 -0500

    fixed fadeOut() shortcut

--- scripts/src/jquery.animate-enhanced.js
+++ scripts/src/jquery.animate-enhanced.js
@@ -194,6 +194,8 @@ Changelog:
 		if (!parts && val == "show") {
 			cleanStart = 1;
 			if (hidden) e.css({'display':'block', 'opacity': 0});
+		} else if (!parts && val == "hide") {
+			cleanStart = 0;
 		}
 
 		if (parts) {
@@ -446,6 +448,11 @@ Changelog:
 						}
 				
 						self.css(reset).css(restore);
+					}
+					
+					// if we used the fadeOut shortcut make sure elements are display:none
+					if (prop.opacity === 'hide') {
+						self.css('display', 'none');
 					}
 			
 					// reset
