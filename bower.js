936844c9753abcc6c2877b1613c1c4a996a3abc3
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-01-15 11:23:32 +0000

    adding bower manifest

---
+++ bower.js
@@ -0,0 +1,14 @@
+{
+  "name": "benbarnett/jQuery-Animate-Enhanced",
+  "version": "1.0.7",
+  "main": "./jquery.animate-enhanced.min.js",
+  "description": "Extend $.animate() to detect CSS transitions for Webkit, Mozilla, IE>=10 and Opera and convert animations automatically.",
+  "license": "MIT (http://opensource.org/licenses/mit-license.php)",
+  "ignore": [
+    ".jshintrc",
+    "**/*.txt"
+  ],
+  "dependencies": {
+    "jquery": ">=1.3.2"
+  }
+}
